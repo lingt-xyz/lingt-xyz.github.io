6b8da18009ede26302f18adc6090417f2a22534c
Ling Tan <lingt.xyz@gmail.com>
2020-07-20 04:07:32 -0400

    update 321 project id

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   courses: {
     INSE: ['course/doc1', 'course/doc2', 'course/doc3'],
-    SOEN: ['course/SOEN321', 'course/SOEN321_project'],
+    SOEN: ['course/SOEN321', 'course/SOEN321_Project'],
   },
   linux: ["linux/Bash"]
 };
