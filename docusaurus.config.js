ac8d1beb2fb2a0b4250db91647f0830c86cbbf0a
Ling Tan <lingt.xyz@gmail.com>
2020-07-25 11:45:13 -0400

    update docs edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -97,7 +97,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/lingt-xyz/io/tree/master/',
           remarkPlugins: [remarkMath],
           rehypePlugins: [[rehypeKatex, {strict: false}]],
         },
