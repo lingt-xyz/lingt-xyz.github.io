9031740e848c6c7d57100780f689d2b45656406d
Ling Tan <lingt.xyz@gmail.com>
2020-07-28 19:02:15 -0400

    remove template from index

--- src/pages/index.js
+++ src/pages/index.js
@@ -60,12 +60,12 @@ function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Notes for everything <head />">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
+          {/* <div className={styles.buttons}>
             <Link
               className={clsx(
                 'button button--outline button--secondary button--lg',
@@ -74,10 +74,10 @@ function Home() {
               to={useBaseUrl('docs/')}>
               Get Started
             </Link>
-          </div>
+          </div> */}
         </div>
       </header>
-      <main>
+      {/* <main>
         {features && features.length > 0 && (
           <section className={styles.features}>
             <div className="container">
@@ -89,7 +89,7 @@ function Home() {
             </div>
           </section>
         )}
-      </main>
+      </main> */}
     </Layout>
   );
 }
