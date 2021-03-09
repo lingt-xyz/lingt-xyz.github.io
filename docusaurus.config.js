3f1cd88e467d10c3247a4ad2e9ed9978fbb12210
Ling Tan <lingt.xyz@gmail.com>
2021-03-09 10:56:07 -0500

    update default course

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,7 +26,7 @@ module.exports = {
       },
       links: [
         {
-          to: 'docs/course/INSE6120',
+          to: 'docs/course/INSE6110',
           activeBasePath: 'docs/course',
           label: 'Course',
           position: 'left',
