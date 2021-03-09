f0722572d925b3c4aa9e7ff8d50431d8667c8836
Ling Tan <lingt.xyz@gmail.com>
2021-03-09 10:55:32 -0500

    remove 20 40

--- sidebars.js
+++ sidebars.js
@@ -3,7 +3,7 @@ module.exports = {
     MATH: ['course/MATH203', 'course/MATH204', 'course/MATH205', 'course/MATH251'],
     MAST: ['course/MAST218'],
     COMP: ['course/COMP232', 'course/COMP354', 'course/COMP361'],
-    INSE: ['course/INSE6110', 'course/INSE6120', 'course/INSE6130', 'course/INSE6140'],
+    INSE: ['course/INSE6110', 'course/INSE6130'],
     SOEN: ['course/SOEN321', 'course/SOEN321_Project'],
     PHIL: ['course/PHIL210']
   },
